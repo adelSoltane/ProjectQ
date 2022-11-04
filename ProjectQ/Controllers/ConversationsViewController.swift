d0a171d74e41d8b40e63262b6cf5bacc14d6eeaa
Adel <as1746a@student.american.edu>
2022-11-04 15:36:55 -0400

    First commit

---
+++ ProjectQ/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectQ
+//
+//  Created by Andrew  on 11/3/22.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
