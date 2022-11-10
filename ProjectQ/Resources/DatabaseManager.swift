47982ce2f86239035d36066f52408c7ef392f716
Adel <as1746a@student.american.edu>
2022-11-10 00:52:24 -0500

    Login and Registration with email working

--- ProjectQ/Resources/DatabaseManager.swift
+++ ProjectQ/Resources/DatabaseManager.swift
@@ -8,6 +8,8 @@
 import Foundation
 import FirebaseDatabase
 
+// class to call the database
+
 final class DatabaseManager {
     
     static let shared = DatabaseManager()
@@ -22,7 +24,11 @@ final class DatabaseManager {
 extension DatabaseManager {
     
     public func userExists(with email: String, completion: @escaping (Bool) -> Void) {
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -33,7 +39,7 @@ extension DatabaseManager {
     
     /// Inserts new user to database
     public func intertUser(with user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name:": user.lastName])
         
@@ -44,5 +50,11 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
   //  let profilePictureUrl: String
 }
