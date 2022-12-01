35d8a98b2cfed13304e3cd34ec2fe9205a1a281a
Adel <as1746a@student.american.edu>
2022-12-01 18:39:37 -0500

    log out option

--- ProjectQ/Controllers/ConversationsViewController.swift
+++ ProjectQ/Controllers/ConversationsViewController.swift
@@ -12,7 +12,7 @@ class ConversationsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .purple
+ 
         
     }
     
--- ProjectQ/Controllers/ProfileViewController.swift
+++ ProjectQ/Controllers/ProfileViewController.swift
@@ -6,24 +6,50 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var tableView: UITableView!
+        
+        let data = ["Log Out"]
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+            tableView.delegate = self
+            tableView.dataSource = self
+            
+        }
+    }
+    
+    extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return data.count
+        }
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = data[indexPath.row]
+            cell.textLabel?.textAlignment = .center
+            cell.textLabel?.textColor = .purple
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+            
+            do {
+                try FirebaseAuth.Auth.auth().signOut()
+                
+                let vc = LoginViewController()
+                let nav = UINavigationController(rootViewController: vc)
+                nav.modalPresentationStyle = .fullScreen
+                present(nav, animated: false)
+            }
+            catch {
+                print("Failed to Log out")
+            }
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
